304895888f0081b1a8facc70d29a0db95025b280
sten <sten@redhat.com>
2013-01-07 22:19:06 -0600

    post-configured push

---
+++ bin/pilconvert.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# EASY-INSTALL-SCRIPT: 'PIL==1.1.7','pilconvert.py'
+__requires__ = 'PIL==1.1.7'
+import pkg_resources
+pkg_resources.run_script('PIL==1.1.7', 'pilconvert.py')
---
+++ bin/rst2odt_prepstyles.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# EASY-INSTALL-SCRIPT: 'docutils==0.10','rst2odt_prepstyles.py'
+__requires__ = 'docutils==0.10'
+import pkg_resources
+pkg_resources.run_script('docutils==0.10', 'rst2odt_prepstyles.py')
